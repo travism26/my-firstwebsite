b03516d2e3477ac28202eee6434f730b7c9c6b61
travis martin <travismartin266@gmail.com>
2014-01-03 00:11:18 -0400

    fixed update.php
    I patched the update.php and added a condition to check if the user is
    logged in and not logged in based off the result it will display the
    login form or not

--- includes/update.php
+++ includes/update.php
@@ -3,28 +3,10 @@
 <?php
 //in here i want to check if the user is logged in and if so show their updates. else get them to log in!
 
+if($user->isLoggedIn()){
+	include 'includes/widgets/loggedin.php';
+} else{
+	include 'includes/widgets/login.php';
+}
 ?>
-	<div class = "login">
-		<form action = "login.php" method = "post" name = "login">
-			<div class = "login">
-				<label for="username">Login:</label>
-				<input type = "text" name = "username" id ="username">
-			</div>
-			
-			<div>
-				<label for="password">Password:</label>
-				<input type="password" name="password" id="password" autocomplete = "off">
-			</div>
-			<div class="field">
-				<label for="remember">
-					<input type="checkbox" name="remember" id="remember" >Remember me
-				</label>
-			</div>
-			<div class = "login">
-				<input type ="submit" value = "Login">
-				<input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
-				<span> or <a href = "register.php" class = "register_link">Register</a></span>
-			</div>
-		</form>
-	</div>
 </section>
