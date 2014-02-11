446ee5246ee435deb3207e02d34736a4d78711d2
travis martin <travismartin266@gmail.com>
2014-02-10 21:45:51 -0400

    preping the admin page

--- admin_page.php
+++ admin_page.php
@@ -7,7 +7,26 @@ if(Session::exists('home')){
 	echo '<p>'.Session::flash('home') .'</p>';
 }
 ?>
+<table>
+	<tr>
+		<td>
+			<h3>Username</h3>
+		</td>
+		<td>
+			<h3>First Name</h3>
+		</td>
+		<td>
+			<h3>Last Name</h3>
+		</td>
+		<td>
+			<h3>Email</h3>
+		</td>
+		<td>
+			<h3>Locked Account</h3>
+		</td>
+	</tr>
 
+</table>
 
 
 
