23c35e9c953b9abac00a38e57fcfa596178a2c4b
travis martin <travismartin266@gmail.com>
2014-03-10 23:35:48 -0300

    adding message page
    This will allow the user to view comments edit the post or delete the
    post

--- message.php
+++ message.php
@@ -4,7 +4,26 @@ require_once 'core/init.php';
 include 'includes/overall/overallHeader.php'; ?>
 
 
-
+			<div class="post">
+				<div class="post_header">
+					<a href="#" class = "register_link">Travis: </a> says:
+					<input type="image" src="images/icons/delete.png" style="float:right;" name="deletePost">
+					<input type="hidden" id="" value="">
+					
+				</div>
+				<div class="post_message">
+					<p>
+					<?php
+						echo "Hello", '<br>';
+					?>
+					</p>
+				</div>
+			</div>
+			<div class="comment">
+				<div class="comment_header">
+					This is a comment!
+				</div>
+			</div>
 
 
 
